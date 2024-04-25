1b80b137ab8a8c6e76bb571d7b8b4cdee25e6d2b
SebastienGadiou <156098111+SebastienGadiou@users.noreply.github.com>
2024-04-25 11:10:44 +0200

    Update from python script

--- currency.js
+++ currency.js
@@ -1,4 +1,4 @@
 
 var eurosValue = 1.00;
-var dollarsValue = 1.066425;
-var czechCrownsValue = 25.244151;
+var dollarsValue = 1.072658;
+var czechCrownsValue = 25.197157;
--- temperature_plot.js
+++ temperature_plot.js
@@ -1,5 +1,5 @@
 
-var dataTemperature = [{'x': ['Prague', 'Washington D.C.', 'Paris'], 'y': [1.0499999523162842, 5.388999938964844, 3.450000047683716], 'type': 'bar', 'name': 'Temperature', 'marker': {'color': '#ff7f0e'}}];
+var dataTemperature = [{'x': ['Prague', 'Washington D.C.', 'Paris'], 'y': [7.099999904632568, 10.58899974822998, 9.699999809265137], 'type': 'bar', 'name': 'Temperature', 'marker': {'color': '#ff7f0e'}}];
 
 var layoutTemperature = {
     title: 'Average Temperature by City',
